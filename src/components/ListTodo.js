561ad02b4d0fc3648724fd007491987da8629499
Pim van Schayik <pimvschayik@live.nl>
2019-07-25 20:52:37 -0700

    feat: seperate TodoItem component

--- src/components/ListTodo.js
+++ src/components/ListTodo.js
@@ -2,9 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
 
-import { removeTodoAction, toggleCompletedAction } from '../actions'
+import TodoItem from './TodoItem'
 
-const ListTodo = ({ todoList, removeTodo, toggleCompleted, isHidden }) => {
+const ListTodo = ({ todoList, isHidden }) => {
   const todoKeys = Object.keys(todoList)
 
   return (
@@ -14,27 +14,9 @@ const ListTodo = ({ todoList, removeTodo, toggleCompleted, isHidden }) => {
         {todoKeys.map(todoKey => {
           if (isHidden && todoList[todoKey].isCompleted) {
             return null
+          } else {
+            return <TodoItem key={todoKey} item={todoList[todoKey]} name={todoKey} />
           }
-
-          return (
-            <li key={todoKey}>
-              <p>{todoKey}</p>
-              <div className='buttons'>
-                <button
-                  className='remove'
-                  onClick={() => removeTodo(todoKey)}
-                >
-                  x
-                </button>
-                <button
-                  className={todoList[todoKey].isCompleted ? 'complete completed' : 'complete'}
-                  onClick={() => toggleCompleted(todoKey)}
-                >
-                  y
-                </button>
-              </div>
-            </li>
-          )
         })}
       </ul>
     </StyledListTodo>
@@ -42,45 +24,12 @@ const ListTodo = ({ todoList, removeTodo, toggleCompleted, isHidden }) => {
 }
 
 const mapStateToProps = ({ todoList }) => ({ todoList })
-const mapDispatchToProps = dispatch => ({
-  removeTodo: name => dispatch(removeTodoAction(name)),
-  toggleCompleted: name => dispatch(toggleCompletedAction(name)),
-})
-export default connect(mapStateToProps, mapDispatchToProps)(ListTodo)
+export default connect(mapStateToProps)(ListTodo)
 
 const StyledListTodo = styled.div`
   margin-top: 80px;
 
   h2 {
     font-size: 24px;
-  }
-
-  ul li {
-    border-bottom: 1px var(--light) solid;
-
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    margin-top: 20px;
-    padding-bottom: 8px;
-  }
-
-  p {
-    color: var(--red);
-    font-size: 16px;
-  }
-
-  button {
-    &.remove {
-      background-color: var(--red);
-    }
-    &.complete {
-      background-color: var(--green);
-      margin-left: 24px;
-      &.completed {
-        opacity: .5;
-      }
-    }
   }
 `
---
+++ src/components/TodoItem.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components'
+import { connect } from 'react-redux'
+
+import { removeTodoAction, toggleCompletedAction } from '../actions'
+
+const TodoItem = ({ name, item, removeTodo, toggleCompleted }) => (
+  <StyledTodoItem>
+    <p>{name}</p>
+    <div className='buttons'>
+      <button
+        className='remove'
+        onClick={() => removeTodo(name)}
+      >
+        x
+      </button>
+      <button
+        className={item.isCompleted ? 'complete completed' : 'complete'}
+        onClick={() => toggleCompleted(name)}
+      >
+        y
+      </button>
+    </div>
+  </StyledTodoItem>
+)
+
+const mapDispatchToProps = dispatch => ({
+  removeTodo: name => dispatch(removeTodoAction(name)),
+  toggleCompleted: name => dispatch(toggleCompletedAction(name)),
+})
+export default connect(null, mapDispatchToProps)(TodoItem)
+
+const StyledTodoItem = styled.li`
+  border-bottom: 1px var(--light) solid;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-top: 20px;
+  padding-bottom: 8px;
+
+  p {
+    color: var(--red);
+    font-size: 16px;
+  }
+
+  button {
+    &.remove {
+      background-color: var(--red);
+    }
+    &.complete {
+      background-color: var(--green);
+      opacity: .5;
+      margin-left: 24px;
+      &.completed {
+        opacity: 1;
+      }
+    }
+  }
+
+`
